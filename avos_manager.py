fbc592cf42f63cbadc77afc469f0e4602b9e5d31
bboalimoe <danielzhang-1@163.com>
2014-10-20 10:05:52 +0800

    add a member method

--- avos_manager.py
+++ avos_manager.py
@@ -8,42 +8,63 @@ import arrow
 import json
 import settings
 import datetime
+import requests
 from util_opt import *
 class AvosClass(AVObject):
     def __init__(self):
         super(AvosClass, self).__init__()
 
 class AvosManager(object):
-	def __init__(self):
-		AvosClass.app_settings = [settings.avos_app_id, settings.avos_app_key]
-	def saveData(self,className,dataDict):
-		res = AvosClass._save_to_avos(className,dataDict)
-		if 'createdAt' not in json.loads(res.content):
-			print res.content
+        def __init__(self):
+                AvosClass.app_settings = [settings.avos_app_id, settings.avos_app_key]
+        def saveData(self,className,dataDict):
+                res = AvosClass._save_to_avos(className,dataDict)
+                if 'createdAt' not in json.loads(res.content):
+                        print res.content
+
+        #New add:get info by specified opt
+        def getData(self,className,**kwargs):
+                with_params = {}
+                for kparam, vparam in kwargs.items():
+                    with_params[kparam] = vparam
+    
+                res = requests.get(
+                    AvosClass.base_classes+className,
+                    headers=AvosClass.headers(),
+                    params=with_params,
+                    verify=False
+                )
+                if 'error' not in json.loads(res.content):
+                    return res.content
 
 if __name__ == "__main__":
-	avosManager = AvosManager()
-	start = "2013-05-05 20:30:45"
-	date_utc = getUtcDate(start)
-	start_utc = timeConvet2utc(start)
-	
-	
-	
-	
-	start_iso = start_utc.replace(" ","T")+".000Z"
-	date_iso = date_utc.replace(" ","T")+".000Z"
-	date_time = dict(__type='Date',iso=date_iso)	
-	start_time = dict(__type='Date',iso=start_iso)
-	end_time = dict(__type='Date',iso=start_iso)
-	dataDict = {"name":"《文成公主》大型实景剧","date":date_time,
-	"start_time":start_time,"end_time":end_time,"ticket":"0","region":"北京市海淀区北京邮电大学","longitude":116.361834,"lattitude":39.970513,"category":""}
-	className = "testDate"
-	avosManager.saveData(className,dataDict)
-	'''
-	AvosClass.app_settings = [settings.avos_app_id, settings.avos_app_key]
-	res = AvosClass.save(dataDict)
-	if 'createdAt' in json.loads(res.content):
-		print '\nSucceeded in creating test object in AvosClass!\n'
-	else:
-		print res.content
-	'''
+        avosManager = AvosManager()
+        r = avosManager.getDataByCol('Beacon',['minor'])
+        print r
+
+if __name__ == "__main__bak":
+        avosManager = AvosManager()
+        start = "2013-05-05 20:30:45"
+        date_utc = getUtcDate(start)
+        start_utc = timeConvet2utc(start)
+        
+        
+        
+        
+        start_iso = start_utc.replace(" ","T")+".000Z"
+        date_iso = date_utc.replace(" ","T")+".000Z"
+        date_time = dict(__type='Date',iso=date_iso)    
+        start_time = dict(__type='Date',iso=start_iso)
+        end_time = dict(__type='Date',iso=start_iso)
+        dataDict = {"name":"《文成公主》大型实景剧","date":date_time,
+        "start_time":start_time,"end_time":end_time,"ticket":"0","region":"北京市海淀区北京邮电大学","longitude":116.361834,"lattitude":39.970513,"category":""}
+        className = "testDate"
+        avosManager.saveData(className,dataDict)
+        '''
+        AvosClass.app_settings = [settings.avos_app_id, settings.avos_app_key]
+        res = AvosClass.save(dataDict)
+        if 'createdAt' in json.loads(res.content):
+                print '\nSucceeded in creating test object in AvosClass!\n'
+        else:
+                print res.content
+        '''
