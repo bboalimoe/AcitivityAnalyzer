9d20c3ab89d8e9a2e2e00f6ae6258da2672f5d19
bboalimoe <danielzhang-1@163.com>
2014-10-22 19:15:58 +0800

    get the timer done

---
+++ runCrawlerTimer.py
@@ -0,0 +1,21 @@
+# -*- encoding:utf-8 -*-
+from damai import DamaiSpider
+from douban import DoubanSpider
+from huodongxing import HuodongxingSpider
+from timer import SchedTimer
+import thread
+
+
+def runCrawlers():
+    damaiSpider = DamaiSpider()
+    doubanSpider = DoubanSpider()
+    huodongxingSpider = HuodongxingSpider()
+    #run crawlling using multi-threads
+    thread.start_new_thread(damaiSpider.crawl,())
+    thread.start_new_thread(doubanSpider.crawl,())
+    thread.start_new_thread(huodongxingSpider.crawl,())
+
+if __name__=="__main__":
+    runCrawlers()
+    t = SchedTimer(24,00,00)
+    t.start(runCrawlers)
